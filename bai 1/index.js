2d6074d48e695f4ee2851eb77eb9ad6863467bc4
Ng-HuuDev <nguyenhuudev0203@gmail.com>
2024-07-28 22:31:26 +0700

    xong bai 1

--- bai 1/index.js
+++ bai 1/index.js
@@ -1,7 +1,9 @@
 function tinhTien() {
   var tien1hTag = document.getElementById("tienluong1h");
-  var sogioTag = document.getElementById("sogiolam");
+  var soGioTag = document.getElementById("sogiolam");
   var sotienluong = tienluong1h.value * sogiolam.value;
   console.log(sotienluong);
-  document.getElementById("result").innerHTML = `Số Tiền Lương: ${sotienluong}`;
+  document.getElementById(
+    "result"
+  ).innerHTML = `<h2 class="text-danger bg-info p-3 mt-3"> Số tiền nhận: ${sotienluong}USD </h2>`;
 }
